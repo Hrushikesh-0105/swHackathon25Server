53db5b35768838cf94f03c5916c503422695f990
Hrushikesh-0105 <mhrushikesh0105@gmail.com>
2025-04-19 13:23:14 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// server.js
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware to parse form-encoded data
+app.use(express.urlencoded({ extended: true }));
+
+// Import routes
+const loginRoute = require('./routes/login');
+
+// Use the routes
+app.use('/api', loginRoute);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
